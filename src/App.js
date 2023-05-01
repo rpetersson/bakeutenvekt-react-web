bab5eb74d98774aad5941b2066464d12a5556617
Robert Petersson <rpetersson@gmail.com>
2023-05-01 16:43:19 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function calculateVolume() {
+  var ingrediense_value = document.getElementById("ingredienses").value
+  console.log("Button pressed:" + ingrediense_value)
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <form name='calculator_form'>
+          <select id="ingredienses" name="ingredienses">
+            <option value="2">mel</option>
+            <option value="3">korn</option>
+            <option value="4">sylt</option>
+            <option value="5">sirup</option>
+          </select>
+          <input type='text' placeholder='Antal gram'></input>
+          <button type='button' onClick={calculateVolume}>submit</button> 
+        </form>
+        <div className='result'>Result</div>
+      </main>
+
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/calculate.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function calculate() {
+  let ingrediense_value = document.getElementById("calculate_form")
+}
+
+
+export default MyComponent;
