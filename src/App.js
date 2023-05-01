59cba2355d24dbb3f3c58214eb12cfafe07b6725
Robert Petersson <rpetersson@gmail.com>
2023-05-01 20:35:30 +0200

    add function to generateSelectItems

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ingredients_data from "./data/ingredients_data_g_per_ml.json";
 
 function calculateVolume() {
-  var ingrediense_value = document.getElementById("ingredienses").value
-  console.log("Button pressed:" + ingrediense_value)
+  var ingrediense_value = document.getElementById("ingredienses").value;
+  console.log("Button pressed:" + ingrediense_value);
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main>
-        <form name='calculator_form'>
-          <select id="ingredienses" name="ingredienses">
-            <option value="2">mel</option>
-            <option value="3">korn</option>
-            <option value="4">sylt</option>
-            <option value="5">sirup</option>
-          </select>
-          <input type='text' placeholder='Antal gram'></input>
-          <button type='button' onClick={calculateVolume}>submit</button> 
-        </form>
-        <div className='result'>Result</div>
-      </main>
+function generateSelectItems() {
+  const select = document.getElementById("ingredienses");
+  for (let index = 0; index < ingredients_data.ingredients.length; index++) {
+    const optionElement = document.createElement("option");
+    optionElement.value = ingredients_data.ingredients[index].density;
+    optionElement.textContent = ingredients_data.ingredients[index].name;
+    select.appendChild(optionElement);
+  }
+}
 
+generateSelectItems()
+
+function App() {
+  
+  return (
+    
+    
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <form name="calculator_form">
+          <select id="ingredienses" name="ingredienses">
+          </select>
+          <input type="text" placeholder="Antal gram"></input>
+          <button type="button" onClick={calculateVolume}>
+            submit
+          </button>
+        </form>
+        <div className="result">Result</div>
+      </main>
     </div>
   );
 }
-
-
 
 export default App;
--- src/components/calculate.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function calculate() {
-  let ingrediense_value = document.getElementById("calculate_form")
-}
-
-
-export default MyComponent;
