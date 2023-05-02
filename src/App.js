756cd2dd82df7edccd6ab38915c364f6bdcfc9d1
Robert Petersson <rpetersson@gmail.com>
2023-05-02 13:46:43 +0200

    add tbsp functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 import ingredients_data from "./data/ingredients_data_g_per_ml.json";
 import React from "react";
 
+function omvandlare(volumeInDl) {
+  let hela = Math.trunc(volumeInDl);
+  let omv = volumeInDl - hela;
+  let msk = omv / 0.15;
+  msk = Math.round(msk);
+  var mskDiv = document.getElementById("msk")
+  mskDiv.textContent = `${msk} tbsp`
+  var resultDiv = document.getElementById("result")
+  resultDiv.textContent = `${hela} dl`
+}
+
+
 function calculateVolume() {
   // volume (in dl) = mass (in g) / density (in g/ml) / 100
   var ingrediense_value = document.getElementById("ingredientSelect").value;
   var form_gram = document.getElementById("form_gram").value
   var volumeInDl =  (form_gram / ingrediense_value) / 100
   var resultDiv = document.getElementById("result")
-  resultDiv.textContent = `${volumeInDl} dl`
+  //resultDiv.textContent = `${volumeInDl} dl`
+  omvandlare(volumeInDl)
 }
+
 
 function App() {
   const [selectedIngredient, setSelectedIngredient] = React.useState("");
@@ -31,17 +45,20 @@ function App() {
       <header className="App-header"></header>
       <main>
         <form name="calculator_form">
+        <input type="text" placeholder="Antal gram" id='form_gram' onChange={handleIngredientChange}></input>
           <select
             id="ingredientSelect"
             value={selectedIngredient}
             onChange={handleIngredientChange}
           >
+            
             <option value="">Select an ingredient</option>
             {ingredientOptions}
           </select>
-          <input type="text" placeholder="Antal gram" id='form_gram' onChange={handleIngredientChange}></input>
+          
         </form>
-        <div id="result">Result</div>
+        <div id="result">0</div>
+        <div id="msk">0</div>
       </main>
     </div>
   );
