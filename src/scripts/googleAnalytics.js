05332071df6a7b18db96449d4447d988836da6cf
Robert Petersson <rpetersson@gmail.com>
2023-05-06 22:47:22 +0200

    update

--- src/scripts/googleAnalytics.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-N805Y32QYG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-N805Y32QYG');
-</script>
